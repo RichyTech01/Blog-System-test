742f95cdd26ced90a3d421b74234825a0502d9d4
Bankole Ona ara <richwaydev01@gmail.com>
2024-11-28 02:59:11 -0800

    first commit

--- src/utils/Storage.tsx
+++ src/utils/Storage.tsx
@@ -1,9 +1,9 @@
-export const saveToLocalStorage = <T>(key: string, data: T): void => {
+export const saveToLocalStorage = (key: string, data: any) => {
     localStorage.setItem(key, JSON.stringify(data));
   };
   
+  export const getFromLocalStorage = (key:string, defaultValue:any) => {
+    const storedData = localStorage.getItem(key);
+    return storedData ? JSON.parse(storedData) : defaultValue;
+  };
   
-  export const getFromLocalStorage = <T>(key: string, defaultValue: T): T => {
-  const storedData = localStorage.getItem(key);
-  return storedData ? JSON.parse(storedData) : defaultValue;
-};
